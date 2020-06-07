284f579bfe8487d2e3648353785c8f347f9f2ecd
dreamsvijay <vijay.a@dreamguys.co.in>
2020-06-07 19:59:13 +0530

    updated new files 7

--- js/login.js
+++ js/login.js
@@ -22,7 +22,7 @@ $(document).ready(function(e){
 	password.keyup(validateLpassword);
 		$('#login-form').submit(function(){ 
 				if(validateEmailUsername() & validateLpassword() )
-				{
+				{ alert(1);
         androidToken = window.localStorage.getItem("androidToken");
         iosToken = window.localStorage.getItem("iosToken");
         var dataString ="uname="+$("#email").val()+"&pass="+$("#password").val()+"&android="+androidToken+"&ios="+iosToken;
@@ -35,7 +35,7 @@ $(document).ready(function(e){
                 $(".loading-mask").css('opacity','0.5');
                // disableBack = true;
             },
-            success:function(data){
+            success:function(data){alert(2);
                // disableBack = false;
 				$(".loading-mask").css('opacity','0');
                 if(data.res==1){
